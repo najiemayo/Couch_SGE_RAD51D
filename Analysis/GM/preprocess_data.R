46324e9355a88d5f9b6e7250e1e9e61c00af5836
najiemayo <153330693+najiemayo@users.noreply.github.com>
2026-08-25 15:31:13 -0500

    Add data preprocessing script for GM analysis
    This file contains code to prepare input data for GM analysis from provided supplementary data, detailing the expected columns in long format.

---
+++ Analysis/GM/preprocess_data.R
@@ -0,0 +1,12 @@
+## this file contains the code to prepare input data for GM analysis from provided supplementary data. 
+# Data will have the following columns in long format
+#      CHROM,
+#      POS,
+#      REF,
+#      ALT,
+#      EventCount,
+#      EventType,
+#      SpliceMax,
+#      Rep,
+#      Time,
+#      Exon
