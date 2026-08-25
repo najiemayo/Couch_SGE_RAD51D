971d13dc0fce4d6a799b48138a269126c1236541
najiemayo <153330693+najiemayo@users.noreply.github.com>
2026-08-25 15:21:30 -0500

    GM pipeline functions

--- Analysis/GM/funs.R
+++ Analysis/GM/funs.R
@@ -1,5 +1,5 @@
 ## =============================================================
-## MM (Mixture Model) pipeline functions
+## GM (Gaussian Model) pipeline functions
 ## =============================================================
 
 library(dplyr)
