0b48b3a11b4bf43c4a733c69a232a6c48c0113d7
najiemayo <153330693+najiemayo@users.noreply.github.com>
2026-08-27 09:29:04 -0500

    Enhance comments for data preparation in preprocess_data.R
    Updated comments to provide more detailed explanations of data preparation steps and column sources.

--- Analysis/GM/preprocess_data.R
+++ Analysis/GM/preprocess_data.R
@@ -1,12 +1,12 @@
-## this file contains the code to prepare input data for GM analysis from provided supplementary data. 
+## this file contains the detailed steps to prepare input data for GM analysis from provided supplementary data. 
 # Data will have the following columns in long format
-#      CHROM,
-#      POS,
-#      REF,
-#      ALT,
-#      EventCount,
-#      EventType,
-#      SpliceMax,
-#      Rep,
-#      Time,
-#      Exon
+#      CHROM, from #CHROM
+#      POS, # from Annotation_hg38
+#      REF, # from REF
+#      ALT, # from ALT
+#      EventCount, # from Rep1_Day14	Rep1_Day5	Rep1_Lib	Rep2_Day14	Rep2_Day5	Rep2_Lib	Rep3_Day14	Rep3_Day5	Rep3_Lib convert to long format
+#      EventType, # from variant class, rename Nonsense to Synonymous and Slient to StopGain
+#      SpliceMax, # maximal value of columns 
+#      Rep, # 1to3
+#      Time, # 0=Lib, 1=Day5, 2=Day14
+#      Exon # from SGE
